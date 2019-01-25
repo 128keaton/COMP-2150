7ce1a19d982a5a1ffc874894412dfa7a19b5c32a
Keaton Burleson <keaton.burleson@me.com>
2019-01-24 19:11:15 -0600

    Most of the complex methods have been fleshed out. Still needs some work on the times(double) method

--- src/com/assignment1/Question7/Complex.java
+++ src/com/assignment1/Question7/Complex.java
@@ -6,57 +6,102 @@ public class Complex {
 
     private static int count = 0;
 
-
-    Complex(double a, double b) {
-
+    // Initializers
+    public Complex(double imaginary, double real) {
+        this.imaginary = imaginary;
+        this.real = real;
+        count++;
     }
 
-    Complex(double a) {
+    public Complex(double imaginary) {
+        this.imaginary = imaginary;
+        this.real = 0.0;
+        count++;
+    }
 
+    public Complex(){
+        this.imaginary = 0.0;
+        this.real = 0.0;
+        count++;
     }
 
     public double getReal() {
-        return real;
+        return this.real;
     }
 
     public double getImaginary() {
-        return imaginary;
+        return this.imaginary;
     }
 
+    // Getters
     public static int getCount() {
         return count;
     }
 
+    // Mutators
     public void setReal(double r) {
-        real = r;
+        this.real = r;
     }
 
     public void setImaginary(double i) {
-        imaginary = i;
+        this.imaginary = i;
     }
 
+    // Object methods
     public Complex plus(Complex c) {
-        return this;
+        Complex n = new Complex();
+        n.imaginary = this.imaginary + c.imaginary;
+        n.real = this.real + c.real;
+        return n;
     }
 
     public Complex minus(Complex c) {
-        return this;
+        Complex n = new Complex();
+        n.imaginary = this.imaginary - c.imaginary;
+        n.real = this.real - c.real;
+        return n;
     }
 
-    public Complex times(Complex c) {
-        return this;
+    public Complex times(Complex z) {
+        Complex n = new Complex();
+
+        // FOIL
+        double realOne = this.real * z.real;
+        double imaginaryOne = this.real * z.imaginary;
+        double imaginaryTwo = this.imaginary * z.real;
+        double imaginaryThree = this.imaginary * z.imaginary;
+        // i2 = -1
+        double realTwo = imaginaryThree * -1;
+
+        n.imaginary = (imaginaryOne + imaginaryTwo);
+        n.real = (realOne + realTwo);
+
+        return n;
     }
 
     public Complex times(double a) {
-        return this;
+        Complex n = new Complex();
+
+        // FOIL
+        double realOne = this.real * a;
+        double imaginaryOne = this.real * a;
+        double imaginaryTwo = this.imaginary * a;
+        double imaginaryThree = this.imaginary * a;
+        // i2 = -1
+        double realTwo = imaginaryThree * -1;
+
+        n.imaginary = (imaginaryOne + imaginaryTwo);
+        n.real = (realOne + realTwo);
+
+        return n;
     }
 
     public static double magnitude(Complex c) {
-        return 0.0;
+        return Math.abs(c.imaginary) + c.real;
     }
 
     public boolean equals(Complex c) {
-        return this == c;
+        return this.imaginary == c.imaginary && this.real == c.real;
     }
 
     public String toString() {
--- src/com/assignment1/Question7/TestComplex.java
+++ src/com/assignment1/Question7/TestComplex.java
@@ -2,4 +2,43 @@ package com.assignment1.Question7;
 
 public class TestComplex {
 
+    public static void main(String[] args) {
+        Complex c1 = new Complex(-4.3, 2.5);
+        Complex c2 = new Complex(4.33, -12.34);
+
+        System.out.println("Number of Complex objects: " + Complex.getCount());
+        System.out.println("c1: " + c1.toString());
+        System.out.println("c2: " + c2.toString());
+
+        System.out.println("Real part of c1:     " + c1.getReal());
+        System.out.println("Imaginary part of c1:     " + c1.getImaginary());
+
+        // Testing Mutators
+        c2.setImaginary(3.0);
+        c2.setReal(-4.0);
+        System.out.println("Testing Mutators - Updated c2: " + c2.toString());
+
+        // Magnitude (wip)
+        System.out.println("c1 magnitude: " + Complex.magnitude(c1));
+        System.out.println("c2 magnitude: " + Complex.magnitude(c2));
+
+        Complex c3 = new Complex(6.5);
+        Complex c4 = new Complex(-4.3, 2.5);
+        System.out.println("c3: " + c3.toString());
+        System.out.println("c4: " + c4.toString());
+        System.out.println("Number of Complex objects: " + Complex.getCount());
+
+        // Equality test
+        System.out.println("Test: c1==c4 => " + c1.equals(c4));
+
+        // Complex arithmetic test
+        System.out.println("**** Complex Arithmetic test ****\n");
+        System.out.println("c1 = " + c1.toString() + "c2 = " + c2.toString());
+        System.out.println("c1 + c2 = " + c1.plus(c2));
+        System.out.println("c1 - c2 = " + c1.minus(c2));
+        System.out.println("c1 * c2 = " + c1.times(c2));
+
+        System.out.println("Number of Complex objects: " + Complex.getCount());
+
+    }
 }
