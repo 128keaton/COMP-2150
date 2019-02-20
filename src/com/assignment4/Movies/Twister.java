94cbca4d4718cc25ee32ea5c999d90107a3ea913
Keaton Burleson <keaton.burleson@me.com>
2019-02-19 22:27:25 -0600

    Finishes assignment 3

---
+++ src/com/assignment4/Movies/Twister.java
@@ -0,0 +1,9 @@
+package com.assignment4.Movies;
+
+import com.assignment4.Movie;
+
+public class Twister extends Movie {
+    public Twister(){
+        super("Twister", "Liability only", "Bill Paxton");
+    }
+}
---
+++ src/com/assignment4/Movies/UncleBuck.java
@@ -0,0 +1,9 @@
+package com.assignment4.Movies;
+
+import com.assignment4.Movie;
+
+public class UncleBuck extends Movie {
+    public UncleBuck(){
+        super("Uncle Buck", "Get in your mouse, and get out of here", "John Candy");
+    }
+}
